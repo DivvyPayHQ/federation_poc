3209b9efdeeda94c207478ac0ddc58f9226d7059
Doruk Gurleyen <dorukgurleyen@gmail.com>
2022-12-22 13:21:27 -0600

    Update inventory subgraph to Federation v2

--- inventory/lib/inventory_web/schema.ex
+++ inventory/lib/inventory_web/schema.ex
@@ -2,8 +2,19 @@ defmodule InventoryWeb.Schema do
   use Absinthe.Schema
   use Absinthe.Federation.Schema
 
+  link(url: "https://specs.apollo.dev/federation/v2.0", import: ["@key", "@extends", "@external"])
+
   query do
     extends()
+  end
+
+  mutation do
+    field :decrement_inventory, :string do
+      arg(:upc, non_null(:string))
+      arg(:amount, :integer, default_value: 1)
+
+      resolve(&resolve_decrement_inventory/2)
+    end
   end
 
   object :product do
@@ -20,4 +31,7 @@ defmodule InventoryWeb.Schema do
   end
 
   defp resolve_in_stock_for_product(%{upc: _upc} = _product, _args, _ctx), do: {:ok, true}
+
+  defp resolve_decrement_inventory(%{upc: upc, amount: amount}, _res),
+    do: {:ok, "Decremented the inventory for UPC #{upc} by #{amount}"}
 end
