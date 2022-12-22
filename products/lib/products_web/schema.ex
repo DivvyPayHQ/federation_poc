104ab996b86c3e4f6fe8efad1a5bbae496a5baa8
Doruk Gurleyen <dorukgurleyen@gmail.com>
2022-12-22 13:44:31 -0600

    Update products to Federation v2

--- products/lib/products_web/schema.ex
+++ products/lib/products_web/schema.ex
@@ -2,12 +2,21 @@ defmodule ProductsWeb.Schema do
   use Absinthe.Schema
   use Absinthe.Federation.Schema
 
-  query do
-    extends()
+  link(url: "https://specs.apollo.dev/federation/v2.0", import: ["@key"])
 
+  query do
     field :product, :product do
       arg(:upc, non_null(:string))
       resolve(&resolve_product_by_upc/2)
+    end
+  end
+
+  mutation do
+    field :add_discount, :string do
+      arg(:upc, non_null(:string))
+      arg(:amount, non_null(:integer))
+
+      resolve(&resolve_add_discount/2)
     end
   end
 
@@ -20,4 +29,7 @@ defmodule ProductsWeb.Schema do
 
   defp resolve_product_by_upc(%{upc: upc}, _ctx),
     do: {:ok, %{upc: upc, name: "Test Product", price: 1000}}
+
+  defp resolve_add_discount(%{upc: upc, amount: amount}, _res),
+    do: {:ok, "Added $#{amount} discount for the item with UPC #{upc}"}
 end
