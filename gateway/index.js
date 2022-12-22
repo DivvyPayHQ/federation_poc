73caeb2f14b30c7b8b20ada98f75c786e9d7ed8a
Doruk Gurleyen <dorukgurleyen@gmail.com>
2022-12-22 12:48:44 -0600

    Upgrade the gateway to Federation v2

--- gateway/index.js
+++ gateway/index.js
@@ -1,24 +1,23 @@
-const { ApolloServer } = require('apollo-server');
-const { ApolloGateway } = require('@apollo/gateway');
+const { ApolloServer } = require("apollo-server");
+const { ApolloGateway, IntrospectAndCompose } = require("@apollo/gateway");
 
-// Initialize an ApolloGateway instance and pass it an array of
-// your implementing service names and URLs
+// Initialize an ApolloGateway instance and pass it the list your subgraph names and URLs
 const gateway = new ApolloGateway({
-  serviceList: [
-    { name: 'products', url: 'http://localhost:4001/' },
-    { name: 'reviews', url: 'http://localhost:4002/' },
-    { name: 'inventory', url: 'http://localhost:4003/' },
-  ],
+  supergraphSdl: new IntrospectAndCompose({
+    subgraphs: [
+      { name: "products", url: "http://localhost:4001/" },
+      { name: "reviews", url: "http://localhost:4002/" },
+      { name: "inventory", url: "http://localhost:4003/" },
+    ],
+  }),
   debug: true,
 });
 
 // Pass the ApolloGateway to the ApolloServer constructor
-const server = new ApolloServer({
-  gateway,
-  subscriptions: false,
-});
+const server = new ApolloServer({ gateway });
 
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Gateway ready at ${url}`);
 }).catch(err => { console.error(err) });
+1
